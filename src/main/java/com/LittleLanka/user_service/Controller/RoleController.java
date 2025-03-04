ba450b1abe43837d8f1626bceb6042b04be9b915
dihaxn <ihanlaknuka@gmail.com>
2025-03-04 18:59:49 +0530

    Create endpoint to get users by the status(Active/Inactive)

--- src/main/java/com/LittleLanka/user_service/Controller/RoleController.java
+++ src/main/java/com/LittleLanka/user_service/Controller/RoleController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@CrossOrigin // Enables Cross-Origin Resource Sharing
+@CrossOrigin("http://localhost:5173/")
 @RequestMapping("api/v1/role")
 public class RoleController {
 
--- src/main/java/com/LittleLanka/user_service/Service/UserService.java
+++ src/main/java/com/LittleLanka/user_service/Service/UserService.java
@@ -5,6 +5,7 @@ import com.LittleLanka.user_service.DTOs.request.RequestLoginDto;
 import com.LittleLanka.user_service.DTOs.request.RequestSaveUserDTO;
 import com.LittleLanka.user_service.DTOs.response.ResponseUserDto;
 import com.LittleLanka.user_service.DTOs.response.ResponseUserWithPermissionsDto;
+import com.LittleLanka.user_service.Entities.enums.UserStatus;
 
 import java.util.List;
 
@@ -21,4 +22,6 @@ public interface UserService {
     ResponseUserDto getUserByUserNamPwd(RequestLoginDto requestLoginDto);
 
     ResponseUserWithPermissionsDto getUserWithPermissionsById(Long userId);
+
+    List<ResponseUserDto> getUsersByStatus(UserStatus status);
 }
